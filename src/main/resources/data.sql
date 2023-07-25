14d7b30dc58d99e2544cb65f6d597ca68348b4fd
endlessmomo <soohyuk96@naver.com>
2023-07-25 11:26:22 +0900

    feat : data sql 정의

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into student(student_number, student_name, email, track) values("1000000", "엄수혁", "soohyuk96@naver.com", "Cloud Engineering");
+insert into student(student_number, student_name, email, track) values("1000001", "김영현", "1@naver.com", "Cloud Engineering");
+insert into student(student_number, student_name, email, track) values("1000002", "이다인", "2@naver.com", "Cloud Engineering");
+insert into student(student_number, student_name, email, track) values("1000003", "이상혁", "3@naver.com", "Cloud Engineering");
